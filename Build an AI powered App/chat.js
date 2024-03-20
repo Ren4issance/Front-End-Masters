a075535bf7ffe7b69f104d86377b5d1e827be630
Ren4issance <antoine.arnaud63@gmail.com>
2024-03-20 18:38:40 +0100

    formating

--- Build an AI powered App/chat.js
+++ Build an AI powered App/chat.js
@@ -1,43 +1,42 @@
 import { openai } from "./openai.js";
-import readline from "node:readline"
+import readline from "node:readline";
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const newMessage = async (history, message) => {
-    const results = await openai.chat.completions.create({
-        model: "gpt-4-1106-preview",
-        messages: [...history, message]
-    })
-    return results.choices[0].message;
-}
+  const results = await openai.chat.completions.create({
+    model: "gpt-4-1106-preview",
+    messages: [...history, message],
+  });
+  return results.choices[0].message;
+};
 
 const formatMessage = (userInput) => ({ role: "user", content: userInput });
 const chat = () => {
-    const history = [
-        {
-            role: "system", content: "You are an AI assistant. Answer questions or else!",
-        },
-    ]
-    const start = () => {
-     rl.question("You : ", async (userInput) => {
-        if(userInput.toLowerCase() === "exit") {
-            rl.close()
-            return
-        }
-        const message = formatMessage(userInput);
-        const response = await newMessage(history, message);
+  const history = [
+    {
+      role: "system",
+      content: "You are an AI assistant. Answer questions or else!",
+    },
+  ];
+  const start = () => {
+    rl.question("You : ", async (userInput) => {
+      if (userInput.toLowerCase() === "exit") {
+        rl.close();
+        return;
+      }
+      const message = formatMessage(userInput);
+      const response = await newMessage(history, message);
 
-        history.push(message, response)
-        console.log(`\n\nAI: ${response.content}`);
-        start();
-     })
-    }
-start();
-}
-console.log(("Chatbot initialized. Type 'exit' to end the chat."));
+      history.push(message, response);
+      console.log(`\n\nAI: ${response.content}`);
+      start();
+    });
+  };
+  start();
+};
+console.log("Chatbot initialized. Type 'exit' to end the chat.");
 chat();
-
